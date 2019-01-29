78a1ce5095e35e78a657dcbadebad6fb67f911bc
Hannah Fisher <fisher97@uw.edu>
2019-01-29 15:25:10 -0800

    Create constants.h
    constants

---
+++ constants.h
@@ -0,0 +1,25 @@
+#indef _CONSTANTS_H
+#define _CONSTANTS_H
+
+
+#define BUF_LENGTH 16
+
+
+#define SPEC_LOW 500
+#define SPEC_HIGH 1000
+#define EVNT_LOW 0
+#define EVNT_HIGH 9999
+
+//constants for display
+#define DISPLAY_MAIN 0
+#define DISPLAY_MENU 1
+#define DISPLAY_MEASURE 2
+//constants for communications
+
+#define COMM_FREQ 0
+#define COMM_PRD 1
+#define COMM_TIME 2
+#define COMM_SPEC 3
+#define COMM_EVNT 4
+
+#endif
