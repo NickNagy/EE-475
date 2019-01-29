e01e8f99daa2229756c184e8f1d833b2bda44002
Hannah Fisher <fisher97@uw.edu>
2019-01-29 15:43:57 -0800

    Update constants.h
    changes

--- constants.h
+++ constants.h
@@ -4,16 +4,44 @@
 
 #define BUF_LENGTH 16
 
+//measurement max and min values
 
+//spectrum and event
 #define SPEC_LOW 500
 #define SPEC_HIGH 1000
 #define EVNT_LOW 0
 #define EVNT_HIGH 9999
 
+//frequency
+#define FREQ_H_MAX 1000000
+#define FREQ_H_LOW 0
+#define FREQ_L_MAX 100
+#define FREQ_L_LOW 0
+#define FREQ_H_RANGE 1000
+#define FREQ_L_RANGE 0.1
+
+//period
+#define PER_H_MAX 0.01
+#define PER_H_LOW 0
+#define PER_L_MAX 1
+#define PER_L_LOW 0
+#define PER_H_RANGE 0.00001
+#define PER_L_RANGE 0.01
+
+//time-interval
+#define TIME_H_MAX 0.01
+#define TIME_H_LOW 0
+#define TIME_L_MAX 1
+#define TIME_L_LOW 0
+#define TIME_H_RANGE 0.00001
+#define TIME_L_RANGE 0.01
+
+
+
 //constants for display
 #define DISPLAY_MAIN 0
 #define DISPLAY_MENU 1
-#define DISPLAY_MEASURE 2
+#define DISPLAY_MEASUREMENTS 2
 //constants for communications
 
 #define COMM_FREQ 0
