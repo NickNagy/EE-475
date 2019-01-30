d0a32713551ef6fbc7995e5ea872a786f4110494
NickNagy <nickvincentnagy@gmail.com>
2019-01-29 17:22:32 -0800

    simplify, RX clears

--- main.c
+++ main.c
@@ -5,15 +5,12 @@
  * Created on January 26, 2019, 2:54 PM
  */
 
-
 #include <xc.h>
 #include "config.h"
 #include "pic18.h"
 #include "pic18f452.h"
 #include "stdio.h"
 #include "constants.h"
-
-
 
 // globals, helps free up space by putting samples om the heap
 int samples[256];
@@ -26,36 +23,34 @@ void main(void) {
 	BRGH = 1;
 
 	counter = 0;
-	unsigned int baud_rate = convert_baud_rate();
 
-	// may need to convert to binary representation?
-	SPBRG = baud_rate; // convert_baud_rate();
-					   // SSPADD = 9600;
-					   // http://www.microcontrollerboard.com/pic_serial_communication.html
+	// http://www.microcontrollerboard.com/pic_serial_communication.html
+	SPBRG = convert_baud_rate();
+					   
 	TRISB = 0;
 	PORTB = 0;
 	// TXSTA = 0b00100010; // determining settings for the transmitter
 	// RCSTA = 0b10010000; // determining settings for the receiver
 	SSPSTAT[7] = 1; // disable slew rate
 
-					// configuration for A/D converters
+	// configuration for A/D converters
 	ADCON0bits.CHS = 0b000;
 	ADCON1bits.ADFM = 1; // 1 = right-justified, 0 = left-justified
 	ADCON1bits.ADCS2 = 1;
 	ADCON1bits.PCFG = 0b1001;
 
 	clearTX();
-    char imaginary[256] = {0};
+    char imaginary[256] = {0}; // for FFT
 
 	while (1) {
-		// measure
+		clearRX(); // should RX send nothing b/w measurements??
 		if (TXREG != 0) {
 			while (ADCON0bits.GO_NOT_DONE); // wait til done reading
 			int singleSample = (ADRESH << 8) + ADRESL;
             if (TXREG == COMM_FREQ) {
-                samples[counter] = singleSample;// High or low??
+                samples[counter] = singleSample;
                 if (counter == 255) {
-                    TXREG = 0;
+                    clearTX();
                     RXREG = optfft(samples, imaginary);
                 }
                 counter = (counter + 1) % 255;
@@ -77,6 +72,10 @@ unsigned int convert_baud_rate() {
 		factor = 64;
 	}
 	return int(_XTAL_FREQ / (factor*(DESIRED_BR)) - 1);
+}
+
+void clearRX() {
+	RXREG = 0x0;
 }
 
 void clearTX() {
