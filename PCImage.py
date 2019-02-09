a1a591d7fd4e9414be09db226c6ccfc74566f53f
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2019-02-09 14:16:15 -0800

    Add files via upload

---
+++ PCImage.py
@@ -0,0 +1,16 @@
+import cv2
+from scipy.misc import imresize
+
+SAVE_PATH = "D:/PCImages/"
+
+cap = cv2.VideoCapture(0)
+
+counter = 0
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) == ord('q'):
+        break
+    cv2.imwrite(SAVE_PATH + str(counter) + ".jpg", imresize(gray, (224, 224)))
+    counter += 1
