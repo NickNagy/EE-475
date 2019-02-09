75391f2f980d211ca8211f3a1787379665a8b62f
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2019-02-09 13:20:23 -0800

    Update boundingBox.py

--- boundingBox.py
+++ boundingBox.py
@@ -3,7 +3,7 @@ import numpy as np
 import csv
 import os
 
-IMAGE_SAVE_PATH = 'D:/ArduinoImages/'
+IMAGE_SAVE_PATH = 'D:/PCImages/'
 CSV_SAVE_PATH = IMAGE_SAVE_PATH
 
 os.chdir(IMAGE_SAVE_PATH)
@@ -15,7 +15,12 @@ x2 = 0
 y1 = 0
 y2 = 0
 
-csvfile = open(CSV_SAVE_PATH + 'data.csv', 'w')
+counter = 205
+
+if counter:
+    csvfile = open(CSV_SAVE_PATH + 'data.csv', 'a') # don't overwrite pre-existing saved data
+else:
+    csvfile = open(CSV_SAVE_PATH + 'data.csv', 'w')
 
 def reset_coords():
     global x1, y1, x2, y2
@@ -39,9 +44,9 @@ def redefine_coordinates(x1, y1, x2, y2):
 # x1, y1, x2, y2
 def writeLine(csvfile, image_path, x1, y1, x2, y2):
     if x1 == x2 or y1 == y2:
-        coord_str = ',,,,\n'
+        coord_str = ',,,,0\n'
     else:
-        coord_str = str(x1) + ',' + str(y1) + ',' + str(x2) + ',' + str(y2) + ', finger\n'
+        coord_str = str(x1) + ',' + str(y1) + ',' + str(x2) + ',' + str(y2) + ',1\n'
     csvfile.write(image_path + ',' + coord_str)
 
 def draw_box(event, x, y, flags, param):
@@ -59,30 +64,27 @@ def draw_box(event, x, y, flags, param):
         box_exists = True
 
 def label_image():
-    global box_exists, image, counter, x1, y1, x2, y2
+    global box_exists, image, counter, x1, y1, x2, y2, csvfile
     clone = image.copy() # save copy of image in case need to re-draw box
     while True:
         cv2.imshow('', image)
         cv2.setMouseCallback('', draw_box)
-        if cv2.waitKey(1) == 8: # backspace
+        key = cv2.waitKey(1)
+        if key == 8: # backspace
             image = clone.copy()
             reset_coords()
             box_exists = False
-        if cv2.waitKey(1) == 13: # enter
+        if key == 13: # enter
             print("writing " + str(counter) + " to csv...")
-            writeLine(csvfile, IMAGE_SAVE_PATH + str(counter) + '.png', x1, y1, x2, y2)
+            writeLine(csvfile, IMAGE_SAVE_PATH + str(counter) + '.jpg', x1, y1, x2, y2)
             counter += 1
             reset_coords()
             box_exists = False
             break
 
-
-counter = 0
-
-# assumes data is saved as [integer].jpg
 while True:
-    image = cv2.imread(str(counter) + '.png')
-    if image is None:
+    image = cv2.imread(str(counter) + '.jpg')
+    if image is None or cv2.waitKey(0) == 27:
         csvfile.close()
         break
     label_image()
