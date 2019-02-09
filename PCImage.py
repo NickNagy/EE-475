97b99cde0aab960f908897a4a03e4203dd8cb1ed
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2019-02-09 14:20:31 -0800

    Update PCImage.py

--- PCImage.py
+++ PCImage.py
@@ -10,7 +10,7 @@ while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', gray)
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == 27:
         break
     cv2.imwrite(SAVE_PATH + str(counter) + ".jpg", imresize(gray, (224, 224)))
     counter += 1
