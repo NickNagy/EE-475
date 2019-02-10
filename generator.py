a578018c3ea5d100e37248c4cf90d348a18e2f27
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2019-02-09 16:51:39 -0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,34 +1,43 @@
 import numpy as np
-
+from random import shuffle
 from PIL import Image
 
 class Generator(object):
 
-    def __init__(self, file_path, one_hot=True):
+    def __init__(self, file_path, one_hot=True, shuffle_data=True):
         file = open(file_path, "r")
         image_path, x1, y1, x2, y2, label = file.readline().split(',')
-        coordinates = np.array([int(x1), int(y1), int(x2), int(y2)])
+        coordinates = np.array([None, None, None, None])
+        if x1 != '':
+            coordinates = np.array([int(x1), int(y1), int(x2), int(y2)])
         self.data_list = []
         self.idx = 0
+        self.shuffle_data = shuffle_data
         self.one_hot = one_hot
         # issue if used on a multi-class problem
         while True:
             try:
                 while True:
-                    new_image_path, x1, y1, x2, y2, new_label = csvfile.readline().split(',')
+                    new_image_path, x1, y1, x2, y2, new_label = file.readline().split(',')
                     if new_image_path != image_path:
-                        self.data_list.append(image_path, coordinates, label)
-                        coordinates = np.array([int(x1), int(y1), int(x2), int(y2)])
+                        self.data_list.append((image_path, coordinates, label))
+                        if x1 != '':
+                            coordinates = np.array([int(x1), int(y1), int(x2), int(y2)])
                         image_path = new_image_path
                         label = new_label
-                    coordinates = np.append(coordinates, [[int(x1), int(y1), int(x2), int(y2)]], axis=0)
+                        break
+                    if x1 != '':
+                        coordinates = np.append(coordinates, [[int(x1), int(y1), int(x2), int(y2)]], axis=0)
             except ValueError:
                 break
         file.close()
 
+    def length(self):
+        return len(self.data_list)
+
     def _load_data_and_label(self):
         data, label = self._next_data()
-        labels = [~label, label]
+        labels = np.array([~label, label])
         return data.reshape(1, data.shape[0], data.shape[1], 1), labels.reshape(1,1,1,2)
 
     def __call__(self, n):
@@ -47,15 +56,22 @@ class Generator(object):
             X[i] = data
             Y[i] = labels
         return X,Y
-        
+
     def _load_image(self, path, dtype=np.float32):
         return np.array(Image.open(path), dtype)
 
+    def _cycle(self):
+        self.idx += 1
+        if self.idx >= len(self.data_list):
+            self.idx = 0
+            if self.shuffle_data:
+                shuffle(self.data_list)
+
     def _next_data(self):
-        self._cycle_file()
+        self._cycle()
         image_path = self.data_list[self.idx][0]
-        image = self._load_image(self, image_path, np.float32)
-        label = self.data_list[self.idx][2]
+        image = self._load_image(image_path, np.float32)
+        label = int(self.data_list[self.idx][2])
         if not self.one_hot:
             boxes = self.data_list[self.idx][1]
             return image, boxes, label
