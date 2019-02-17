a0b31aaa81b87301dbfa9a948141971366df34c3
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2019-02-17 15:30:57 -0800

    ghost registers

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-/*
  * File:   main.c
  * Author: Nick Nagy
  *
@@ -26,11 +25,32 @@
 #define COMM_SPEC 4
 #define N 256
 
+union {
+    unsigned char all; // since technically not a byte
+    struct {
+        unsigned R0: 1;
+        unsigned R1: 1;
+        unsigned R2: 1;        
+    } bits;
+} tempEPort;
+
+union {
+    unsigned char all;
+    struct {
+        unsigned R0: 1;
+        unsigned R1: 1;
+        unsigned R2: 1;
+        unsigned R3: 1;
+        unsigned R4: 1;
+        unsigned R5: 1;
+    } bits;
+} tempAPort;
+
 // define pins
-#define NOT_OE PORTEbits.RE0
-#define NOT_WE PORTEbits.RE1
-#define COUNTER_RESET PORTAbits.RA1
-#define COUNTER_CLK PORTAbits.RA2
+#define NOT_OE tempEPort.bits.R0 //LATE0
+#define NOT_WE tempEPort.bits.R1 //LATE1
+#define COUNTER_RESET tempAPort.bits.R1//LATA1
+#define COUNTER_CLK tempAPort.bits.R2//LATA2
 
 // globals, helps free up space by putting samples om the heap
 int samples[N];
@@ -93,20 +113,28 @@ void clearTX() {
 }
 
 void startCounter() {
-    wait();
+    //wait();
     COUNTER_RESET = 0;
+    LATA = tempAPort.all;
+    __delay_ms(1);
 }
 
 void resetCounter() {
-    wait();
+    //wait();
     COUNTER_RESET = 1;
+    LATA = tempAPort.all;
+    __delay_ms(1);
 }
 
 void updateAddress(){
-    wait();
+    //wait();
     COUNTER_CLK = 1;
-    wait();
+    LATA = tempAPort.all;
+    __delay_ms(1);
+    //wait();
     COUNTER_CLK = 0;
+    LATA = tempAPort.all;
+    __delay_ms(1);
 }
 
 void goToAddress(char address) {
@@ -119,26 +147,44 @@ void goToAddress(char address) {
 
 void writeToCurrAddress(char data){
     NOT_OE = 1; // disable output
-    PORTD = data; 
+    LATE = tempEPort.all;
+    __delay_ms(1);
+    LATD = data; 
+    __delay_ms(1);
     NOT_WE = 0;
+    LATE = tempEPort.all;
+    __delay_ms(1);
     NOT_WE = 1;
+    LATE = tempEPort.all;
+    __delay_ms(1);
     updateAddress();
 }
 
 void writeToSpecAddress(char address, char data){
     NOT_OE = 1; // disable output
+    LATE = tempEPort.all;
+    __delay_ms(1);
     goToAddress(address);
-    PORTD = data; // load data into PORTD
+    LATD = data; // load data into PORTD
+    __delay_ms(1);
     NOT_WE = 0; // enable write
+    LATE = tempEPort.all;
+    __delay_ms(1);    
     NOT_WE = 1; // disable write
+    LATE = tempEPort.all;
+    __delay_ms(1);
     updateAddress();
 }
 
 char readFromCurrAddress(){
     NOT_WE = 1;
     NOT_OE = 0;
+    LATE = tempEPort.all;
+    __delay_ms(1);
     char data = PORTD;
     NOT_OE = 1;
+    LATE = tempEPort.all;
+    __delay_ms(1);
     updateAddress();
     return data;
 }
@@ -146,10 +192,16 @@ char readFromCurrAddress(){
 char readFromSpecAddress(char address){
     NOT_OE = 1; // disable output
     NOT_WE = 1; // disable write
+    LATE = tempEPort.all;
+    __delay_ms(1);
     goToAddress(address);
     NOT_OE = 0; // enable output
+    LATE = tempEPort.all;
+    __delay_ms(1);
     char data = PORTD;
     NOT_OE = 1; // disable output
+    LATE = tempEPort.all;
+    __delay_ms(1);
     updateAddress();
     return data;
 }
@@ -161,6 +213,8 @@ void testRAM(){
         writeToCurrAddress(i);
     }
     //char data = 0;
+    resetCounter();
+    startCounter();
     for (char j = 0; j < 32; j++) {
         SRAMdata = readFromCurrAddress();
     }
@@ -222,8 +276,8 @@ void main(void) {
     init();
 
 	while (1) {
-        startCounter();
-        //testRAM();
+        //startCounter();
+        testRAM();
         //testRXTX();
         //writeTX(255);
         /*ADCON0bits.GO_nDONE = 1;
