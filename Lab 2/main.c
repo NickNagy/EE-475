1e04b0ed8051fdd51d69be75b2c4838850571141
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2019-02-18 17:04:36 -0800

    fixed spi and txrx stuff

--- Lab 2/main.c
+++ Lab 2/main.c
@@ -76,12 +76,13 @@ unsigned int convert_baud_rate() {
 		factor = 64;
         SSPCON1bits.SSPM = 0b0010;
 	}
-	return (unsigned int)((unsigned long)_XTAL_FREQ / (factor*(DESIRED_BR)) - 1);
+	return _XTAL_FREQ / (factor*(DESIRED_BR)) - 1;
 }
 
 char getRX(){
     while(PIR1bits.RCIF==0);
-    return(RCREG);
+    char data = RCREG;
+    return data;
 }
 
 void wait(){
@@ -89,7 +90,7 @@ void wait(){
 }
 
 void writeTX(char value){
-    while(TRMT==0);
+    while(TXIF==0);
     TXREG = value;
 }
 
@@ -218,6 +219,7 @@ void testRAM(){
 
 void testRXTX(){
     char result = getRX();
+    __delay_ms(1);
     writeVal(result);
     __delay_ms(1);
 }
@@ -240,6 +242,15 @@ void setTRISB(char val){
     TRISBbits.RB3 = val;
     TRISBbits.RB4 = val;
     TRISBbits.RB5 = val;
+    __delay_ms(1);
+}
+
+char testSPI(){
+    setTRISB(0);
+    SPIWrite(1);
+    //setTRISB();
+    char data = SPIRead();
+    return data;
 }
 
 void init(){
@@ -254,26 +265,29 @@ void init(){
     RCSTAbits.RX9 = 0;
     RCSTAbits.SPEN = 1; // enable RX and TX as serial
     RCSTAbits.CREN = 1; // continuous receive
+    // ?
+    RCSTAbits.ADDEN = 1;
+    RCSTAbits.FERR = 0;
+    RCSTAbits.OERR = 0;
 
-    // SPI stuff
+    // SPI and serial stuff
     SSPCON1bits.CKP = 0; // 0: idle clk state = low level
     SSPSTATbits.SMP = 0; // 0: middle, 1: end
     SSPSTATbits.CKE = 1; // which clock edge? (set opposite on slave)
     SSPSTATbits.SMP = 1; // disable slew rate
-    TRISC7 = 0; // slave-select pin
+    TRISC7 = 1; // RX as input
+    TRISC6 = 0; // TX as output
+    TRISC5 = 0; // serial data out
+    TRISC4 = 0; // slave-select pin
+    TRISC3 = 0; // cleared for master 
+    
     setTRISB(1); // PORT B as input
 
     TRISAbits.RA0 = 1;
     TRISAbits.RA1 = 0;
     TRISAbits.RA2 = 0;
     TRISE = 0x0;
-	TRISC = 0x80;
-    // TRISC3 = 0; <- may be the same thing as above??
     SPBRG = convert_baud_rate();
-    
-    // RB0 can function as an external interrupt
-    // RB1: general I/O
-    //PORTBbits.RB1 = 1;
     
 	// configuration for A/D converters
 	ADCON0bits.CHS = 0b000;
@@ -282,15 +296,12 @@ void init(){
 	ADCON1bits.ADFM = 1; // 1 = right-justified, 0 = left-justified
 	ADCON1bits.ADCS2 = 1;
 	ADCON1bits.PCFG = 0b1110;
-
-    //writeLine("Initializing...");
     
     clearTX();
 }
 
 void main(void) {    
     init();
-
 	while (1) {
         //resetCounter();
         //testRAM();
