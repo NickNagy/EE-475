cfa1bec86d62f8834f652370166e303fa6c0bd23
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2019-02-19 04:42:56 -0800

    Update slave_main.c

--- Lab 2/slave_main.c
+++ Lab 2/slave_main.c
@@ -12,54 +12,51 @@
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
-// CONFIG
-#pragma config FOSC = XT   // Oscillator Selection bits (XT oscillator)
-#pragma config WDTE = OFF  // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = OFF // Power-up Timer Enable bit (PWRT disabled)
-#pragma config BOREN = OFF // Brown-out Reset Enable bit (BOR disabled)
-#pragma config LVP = OFF   // Low-Voltage In-Circuit Serial Programming Enable bit
-#pragma config CPD = OFF   // Data EEPROM Memory Code Protection bit
-#pragma config WRT = OFF   // Flash Program Memory Write Enable bits
-#pragma config CP = OFF    // Flash Program Memory Code Protection bit
-
 #include <xc.h>
-#include <pic16f877a.h>
-#include "spi.h"
+#include <pic18f25k22.h>
+#include "slave_config.h"
+//#include "spi.h"
 
-#define _XTAL_FREQ 8000000
+#define _XTAL_FREQ 16000000UL
 
-void interrupt SPI_Slave_Read()
+char readSPI(){
+    while(!SSPSTATbits.BF);
+    return (SSPBUF);
+}
+
+void writeSPI(char data){
+    TRISC = 0x00; // set PORTC as output
+    SSPBUF = data;
+}
+
+void __interrupt() SPI_Slave_Read()
 {
   if(SSPIF == 1)
   {
-    PORTC = spiRead();
-    spiWrite(PORTB);
+    TRISC = 0xFF; // set as input
+    char data = readSPI();
+    writeSPI(data);
     SSPIF = 0;
   }
 }
 
 void main()
 {
-  nRBPU = 0;    //Enable PORTB internal pull up resistor
-  TRISB = 0xFF; //PORTB as input
-  TRISD = 0x00; //PORTD as output
-  PORTC = 0x00; //All LEDs OFF
-
+  //nRBPU = 0;    //Enable PORTB internal pull up resistor
+  PORTC = 0xFF; 
   GIE = 1;
   PEIE = 1;
   SSPIF = 0;
   SSPIE = 1;
   ADCON1 = 0x07;
-  TRISA5 = 1;
-
-  spiInit(SPI_SLAVE_SS_EN, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE);
+  //TRISA5 = 1;
+  
+  SSP2CON1bits.CKP = 0;
+  SSPSTATbits.CKE = 0;
+  SSPSTATbits.SMP = 1; // disable slew rate
 
   while(1)
   {
-      // Set interrupt
-      // Call SPI_Slave_Read
-      // Call SPIWrite
-      //Do stuff here
-        __delay_ms(5);
+    __delay_ms(1);
   }
 }
